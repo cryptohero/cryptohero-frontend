cb31c775ae130f1f2aac8d38abe859258bc7aa64
Frank Wei <frank@frankwei.xyz>
2018-04-03 23:03:54 +0800

    Add "Exchange LCY" feature

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,8 @@ export const network = {
     default: true, // It will be used when no metaMask
     name: 'Main Ethereum Network',
     contract: '0xd0792ac0de7ef31197c5f452b21a34389ecc725f',
+    convert: '',
+    luckyPackage: '',
     rpc: 'https://mainnet.infura.io/lTETGFVyQX99UKQ98BN4',
   },
   2: {
@@ -14,8 +16,12 @@ export const network = {
     contract: '',
   },
   3: {
+    default: true, // It will be used when no metaMask
     name: 'Ropsten Test Network',
-    contract: '',
+    contract: '0xD62ccC23C5e1Db41C017913303bdB09e570284f6',
+    convert: '0xE3F437609E97246734aa6A550E407E14dB2f53b1',
+    luckyPackage: '0xDC060a83C8A81a029676e85DEe8c0D090b52F2dD',
+    DecentralizedExchangeHotPotato: '0x4a11d94715194d602f9ec570e446e33bf9e41fed',
     rpc: 'https://ropsten.infura.io/lTETGFVyQX99UKQ98BN4',
   },
   4: {
@@ -25,7 +31,10 @@ export const network = {
   },
   42: {
     name: 'Kovan Test Network',
-    contract: '',
+    contract: '0x39e936c591c1990b48d716933b5017e1c09d5327',
+    LuckyPackage: '0x929cc163230081ff517ccc3932c9fd70d00aaccb',
+    Convert: '0xb55edc9c72d56447d6d1ea2476bcb42e74df16cf',
+    DecentralizedExchangeHotPotato: '0xac7e1a3263e06329eee8ac5b45f3ac2223119779',
     rpc: 'https://kovan.infura.io/lTETGFVyQX99UKQ98BN4',
   },
 };
