ae8049f79b8f2ff760cdbf8469df3ae24999f284
Frank Wei <frank@frankwei.xyz>
2018-04-05 14:26:40 +0800

    Kovan network supported

--- src/config/index.js
+++ src/config/index.js
@@ -31,9 +31,9 @@ export const network = {
   },
   42: {
     name: 'Kovan Test Network',
-    contract: '0x39e936c591c1990b48d716933b5017e1c09d5327',
-    LuckyPackage: '0x929cc163230081ff517ccc3932c9fd70d00aaccb',
-    Convert: '0xb55edc9c72d56447d6d1ea2476bcb42e74df16cf',
+    contract: '0x23f9d870fbee3161443d9898d0c494ce0416b886',
+    luckyPackage: '0x929cc163230081ff517ccc3932c9fd70d00aaccb',
+    convert: '0xb55edc9c72d56447d6d1ea2476bcb42e74df16cf',
     DecentralizedExchangeHotPotato: '0xac7e1a3263e06329eee8ac5b45f3ac2223119779',
     rpc: 'https://kovan.infura.io/lTETGFVyQX99UKQ98BN4',
   },
